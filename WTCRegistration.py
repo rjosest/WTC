fa9f67d9c4585e2c70e5059fd7f2a4d6d1fa5cf8
Raul San Jose <rjosest@gmail.com>
2015-01-23 19:20:38 -0500

    ENH: Improving registration

--- WTCRegistration.py
+++ WTCRegistration.py
@@ -13,8 +13,10 @@ parser.add_option("-f",dest="fixedCID",help="case ID for fixed image")
 parser.add_option("-m",dest="movingCID",help="case ID for moving image")
 parser.add_option("-s",dest="sigma",help="sigma for initial smoothing [default: %default]", type="float",default=1)
 parser.add_option("-r",dest="rate",help="initial upsampling/downsampling rate [default: %default]", type="float", default=0.5)
+parser.add_option("-d",dest="dimension",help="dimensionality of the data [default: %default]", type="integer", default=3)
 parser.add_option("--tmpDir",dest="tmpDirectory",help="temporary directory" )
 parser.add_option("--dataDir", dest="dataDirectory",help="data directory for the case IDs")
+parser.add_option("--fast",dest="fast",help="fast execution [default: %default]",action="store_true",default=False)
 
 (options, args) = parser.parse_args()
 
@@ -24,6 +26,8 @@ sigma = options.sigma
 rate = options.rate
 tmp_dir = options.tmpDirectory
 data_dir = options.dataDirectory
+dimension = options.dimension
+fast=options.fast
 
 #Check required tools path enviroment variables for tools path
 toolsPaths = ['ANTS_PATH','TEEM_PATH','ITKTOOLS_PATH'];
@@ -90,44 +94,51 @@ for kk,im in enumerate([fixed_mask,moving_mask]):
   sys.stdout.flush()
   subprocess.call( tmp_command, shell=True)
 
+
 #Perform Affine registration
-percentage=0.2
-its="100x50x20,1e-6,5"
-tmp_command = "antsRegistration -d 3 -r [ %(f)s,%(m)s,1] \
+if fast == True:
+  percentage=0.1
+  its=10000x0x0
+  syn="100x0x0,0,5"
+else:
+  percentage=0.3
+  its="10000x5000x2000"
+  syn="100x100x50,-0.01,5"
+
+tmp_command = "antsRegistration -d %(dim)s -r [ %(f)s,%(m)s,0] \
                -m mattes[  %(f)s, %(m)s , 1 , 32, regular, %(percentage)f ] \
                -t  translation[ 0.1 ] \
                -c [%(its)s,1.e-8,20]  \
                -s 4x2x1vox \
-               -f 8x4x2 \
+               -f 6x4x2 \
                -l 1 \
                -m mattes[  %(f)s, %(m)s , 1 , 32, regular, %(percentage)f ] \
                -t affine[ 0.1 ] \
                -c [%(its)s,1.e-8,20]  \
                -s 4x2x1vox  \
-               -f 8x4x2 \
+               -f 3x2x1 \
                -l 1 -u 1 -z 1 \
                -o [%(nm)s]"
 tmp_command = tmp_command % {'f':fixed_tmp,'m':moving_tmp, \
-'percentage':percentage,'its':its,'nm':deformation_prefix}
+'percentage':percentage,'its':its,'nm':deformation_prefix,'dim':dimension}
 tmp_command = os.path.join(path['ANTS_PATH'],tmp_command)
 print tmp_command
 sys.stdout.flush()
 subprocess.call( tmp_command, shell=True, env=os.environ.copy())
 
 #Perform Diffeomorphic registration
-syn="100x50x40,1e-6,5"
-tmp_command = "antsRegistration -d 3 -r %(affine)s \
--m cc[ %(f)s, %(m)s , 0.5 , 2 ] \
+tmp_command = "antsRegistration -d %(dim)s -r %(affine)s \
+-m cc[ %(f)s, %(m)s , 0.5 , 4 ] \
 -t SyN[ .20, 3, 0 ] \
 -c [ %(syn)s ]  \
--s 8x4x2vox  \
--f 8x4x2 \
+-s 2x1x0vox  \
+-f 4x2x1 \
 -x [%(f_mask)s,%(m_mask)s] \
 -l 1 -z 1 \
 -o [%(nm)s,%(out)s,%(out_inv)s]"
 tmp_command = tmp_command % {'affine':affine_tfm, 'f':fixed_tmp, 'm':moving_tmp, \
 'syn':syn,'f_mask':fixed_mask_tmp,'m_mask':moving_mask_tmp, \
-'nm':deformation_prefix,'out':moving_deformed_tmp,'out_inv':fixed_deformed_tmp}
+'nm':deformation_prefix,'out':moving_deformed_tmp,'out_inv':fixed_deformed_tmp,'dim':dimension}
 tmp_command = os.path.join(path['ANTS_PATH'],tmp_command)
 print tmp_command
 sys.stdout.flush()
@@ -137,17 +148,17 @@ subprocess.call( tmp_command, shell=True, env=os.environ.copy())
 # The previous command already outputs the transformed images composed with both
 # transformations for the tmp images used in the registration. Here we deformed the original ones
 
-tmp_command = "antsApplyTransforms -d 3 -i %(m)s -r %(f)s -n linear \
+tmp_command = "antsApplyTransforms -d %(dim)s -i %(m)s -r %(f)s -n linear \
    -t %(elastic-tfm)s -t %(affine-tfm)s -o %(deformed)s"
 tmp_command = tmp_command % {'f':fixed,'m':moving, \
-'elastic-tfm':elastic_tfm,'affine-tfm':affine_tfm,'deformed':moving_deformed_tmp}
+'elastic-tfm':elastic_tfm,'affine-tfm':affine_tfm,'deformed':moving_deformed_tmp,'dim':dimension}
 tmp_command = os.path.join(path['ANTS_PATH'],tmp_command)
 subprocess.call( tmp_command, shell=True, env=os.environ.copy())
 
-tmp_command = "antsApplyTransforms -d 3 -i %(f)s -r %(m)s -n linear \
+tmp_command = "antsApplyTransforms -d %(dim)s -i %(f)s -r %(m)s -n linear \
 -t [%(affine-tfm)s,1] -t %(elastic-inv-tfm)s  -o %(deformed)s"
 tmp_command = tmp_command % {'f':fixed,'m':moving, \
-'elastic-inv-tfm':elastic_inv_tfm,'affine-tfm':affine_tfm,'deformed':fixed_deformed_tmp}
+'elastic-inv-tfm':elastic_inv_tfm,'affine-tfm':affine_tfm,'deformed':fixed_deformed_tmp,'dim':dimension}
 tmp_command = os.path.join(path['ANTS_PATH'],tmp_command)
 subprocess.call( tmp_command, shell=True, env=os.environ.copy())
 
